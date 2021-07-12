479718e0ad694bfc769c52c537f667c67a68e3f5
Jorge Guevara B <jorge.agb@gmail.com>
2021-07-12 17:32:28 -0500

    Mejorando el código

--- miconvertidor.py
+++ miconvertidor.py
@@ -1,61 +1,56 @@
 from funciones import convertir, mensaje
-
 from datetime import date
 
-#ES Almacenamos la fecha actual en la variable hoy con formato de fecha larga 
-#EN Declare variable "hoy" for store current date with large format
-hoy = date.today().strftime("%A %d de %B de %y")
 
-#ES Declaramos e inicializamos variables donde almacenaremos los tipos de moneda
-#EN Declare and set currency names
-moneda1 = "USD"
-moneda2 = "COP"
+def main():
+        #ES Almacenamos la fecha actual en la variable hoy con formato de fecha larga 
+        #EN Declare variable "hoy" for store current date with large format
+        hoy = date.today().strftime("%A %d de %B de %y")
 
-print('''
-******Convertidor de monedas*******
-***********************************
-Fecha: {}
-Moneda 1: {}
-Moneda 2: {}
+        #ES Declaramos e inicializamos variables donde almacenaremos los tipos de moneda
+        #EN Declare and set currency names
+        moneda1 = "USD"
+        moneda2 = "COP"
 
-'''.format(hoy, moneda1, moneda2))
+        print('''
+        ******Convertidor de monedas*******
+        ***********************************
+        Fecha: {}
+        Moneda 1: {}
+        Moneda 2: {}
 
-#ES Capturamos el factor de conversión de la moneda
-#EN Capture conversion factor among currencies
-factorconv = float(input("1 {} equivale en {} a: ".format(moneda1, moneda2)))
+        '''.format(hoy, moneda1, moneda2))
 
-#ES Imprimimos el menú de opciones
-#EN Print options menu and input 
-print("""
+        #ES Capturamos el factor de conversión de la moneda
+        #EN Capture conversion factor among currencies
+        factorconv = float(input("1 {} equivale en {} a: ".format(moneda1, moneda2)))
 
-=== Selecciona una de las siguientes opciones ===
-    (1) Para convertir de {} a {}
-    (2) Para convertir de {} a {}
-*************************************************    
-""".format(moneda1, moneda2, moneda2, moneda1))
+        #ES Imprimimos el menú de opciones
+        #EN Print options menu and input 
+        print("""
 
-opcion = int(input("Escribe una de las opciones: "))
+        === Selecciona una de las siguientes opciones ===
+        (1) Para convertir de {} a {}
+        (2) Para convertir de {} a {}
+        *************************************************    
+        """.format(moneda1, moneda2, moneda2, moneda1))
+
+        opcion = int(input("Escribe una de las opciones: "))
 
 
-if opcion == 1:
-        valor1 = float(input("Valor en {} ".format(moneda1)))
-        resultado = convertir(valor1, factorconv,opcion)
-        mensaje(valor1,resultado,moneda1, moneda2)
-elif opcion == 2:
-        valor1 = float(input("Valor en {} ".format(moneda2)))
-        resultado = convertir(valor1, factorconv,opcion)
-        mensaje(valor1,resultado,moneda2, moneda1)
+        if opcion == 1:
+                valor1 = float(input("Valor en {} ".format(moneda1)))
+                resultado = convertir(valor1, factorconv,opcion)
+                mensaje(valor1,resultado,moneda1, moneda2)
+        elif opcion == 2:
+                valor1 = float(input("Valor en {} ".format(moneda2)))
+                resultado = convertir(valor1, factorconv,opcion)
+                mensaje(valor1,resultado,moneda2, moneda1)
+        else:
+                print("No seleccionaste una opción válida. Vuelve a iniciar")
+
+
+if __name__ == "__main__":
+        main()
 else:
-        print("No seleccionaste una opción válida. Vuelve a iniciar")
-
-
-        
-
-
-
-
-
-
-    
-
-
+        pass
