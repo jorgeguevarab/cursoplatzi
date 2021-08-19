c33742eabeb45b9dcf415b3fe2fedfac30308b16
Jorge Guevara <jorge.agb@gmail.com>
2021-08-19 17:55:24 -0500

    Check point

--- ahorcado.py
+++ ahorcado.py
@@ -17,7 +17,9 @@ def read():
             lineas.append(linea[:-1])                
     return lineas
     
-def dibuja(lista, inicio, fin):
+def dibuja(lista, tlevels):
+    inicio = tlevels[0]
+    fin = tlevels[1]
     filtrado = [elemento for elemento in lista[inicio-1:fin]]
       
     for i in filtrado:
@@ -40,15 +42,18 @@ def fill_list(char, word, current_word):
     # Return list with chars and underscores
     index = 0
     # current_word = ['_ ' for i in range(len(word) - 1)]
-    
+    is_hit = False
     # new_word = ['_ ' for i in enumerate(word)]
     
     for letter in word:
         if letter == char.upper():
             current_word[index] = char.upper()
+            is_hit = True
+            
         index += 1
     os.system("clear")  
-        
+    
+    return is_hit    
 
 def write(frase):
     
@@ -60,26 +65,38 @@ def write(frase):
 
 def run():
     # os.system("clear")
-    # lista = read()
+    hangman = read()
     # dibuja(lista, 61,70)
     
     # print(select_words())
+    levels = [(61, 70), (51, 60), (41, 50), (31, 40), (21, 30), (11, 20), (1, 10)]
+    
+    is_hit = False
     os.system("clear")
     palabra = select_words()
+    
     print(palabra)
     
-    current_word = ['_ ' for i in range(len(palabra)-1)]
-    opportunities = len(palabra) - 1
+    current_word = ['_ ' for i in range(len(palabra))]
+    print("".join(current_word))
     
+    opportunities = 7
+            
+    while opportunities != 0:
+        if palabra.strip() != "".join(current_word).strip():
+            is_hit = fill_list(input("Inserta caracter: "), palabra, current_word)
+            print("".join(current_word).strip())
+            if is_hit != True:
+                opportunities -= 1
+                print("Tienes %s oportunidades\n" % opportunities)
+                
+                dibuja(hangman, levels[opportunities])                
+        else:
+             print("GANASTEEEEE")
+             break
+      
+            
     
-    
-    for char in range(len(palabra)-1):
-        
-        fill_list(input("Inserta caracter: "), palabra, current_word)
-        # print(current_word)
-        print("".join(current_word))
-        opportunities -= 1
-        print("Tienes %s oportunidades" % opportunities)
        
     
 
